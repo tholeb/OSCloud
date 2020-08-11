63c1416a9dbcb6ac0a684416a82b3e254a89480d
tholeb <thomasleb.49@gmail.com>
2020-08-11 23:30:57 +0200

    add: elements and meta

---
+++ assets/resources/meta/head.php
@@ -0,0 +1,34 @@
+<base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/logo/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/logo/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/logo/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/logo/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/logo/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/logo/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/logo/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/logo/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/logo/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/logo/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="assets/img/logo/favicon/ms-icon-144x144.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="YAOSCP - Yet Another Open Source Cloud Project">
+    <meta name="author" content="tholeb">
+    <meta name="keyword" content="os,oscloud, osc, yaoscp, cloud, open source">
+    <meta name="theme-color" content="#ffffff">
+    <link href="assets/css/style.min.css" rel="stylesheet">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171569789-1"></script>
+    <script src="https://www.googleoptimize.com/optimize.js?id=OPT-NW47QHQ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-171569789-1');
+    </script>
---
+++ assets/resources/meta/scripts.php
@@ -0,0 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="assets/js/main.min.js"></script>
+<script src="assets/js/coreui.bundle.min.js"></script>
+<!--[if IE]><!-->
+<script src="assets/js/svgxuse.min.js"></script>
+<!--<![endif]-->
