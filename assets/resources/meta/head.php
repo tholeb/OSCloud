d5f822ce483e6e0109af764c5cf672ca796631f1
tholeb <thomasleb.49@gmail.com>
2020-08-15 01:21:34 +0200

    head: favicons changing depending on the theme

--- assets/resources/meta/head.php
+++ assets/resources/meta/head.php
@@ -1,22 +1,54 @@
 <base href="./">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/logo/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/logo/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/logo/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/logo/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/logo/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/logo/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/logo/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/logo/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/logo/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo/favicon/favicon-16x16.png">
-    <link rel="manifest" href="assets/img/logo/favicon/manifest.json">
+    <!-- black favicons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/logo/favicon/b/apple-icon-57x57.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/logo/favicon/b/apple-icon-60x60.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/logo/favicon/b/apple-icon-72x72.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/logo/favicon/b/apple-icon-76x76.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/logo/favicon/b/apple-icon-114x114.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/logo/favicon/b/apple-icon-120x120.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/logo/favicon/b/apple-icon-144x144.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/logo/favicon/b/apple-icon-152x152.png" media="(prefers-color-scheme:light)">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo/favicon/b/apple-icon-180x180.png" media="(prefers-color-scheme:light)">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/logo/favicon/b/android-icon-192x192.png" media="(prefers-color-scheme:light)">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo/favicon/b/favicon-32x32.png" media="(prefers-color-scheme:light)">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo/favicon/b/favicon-96x96.png" media="(prefers-color-scheme:light)">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo/favicon/b/favicon-16x16.png" media="(prefers-color-scheme:light)">
+    <meta name="msapplication-TileImage" content="assets/img/logo/favicon/b/ms-icon-144x144.png" media="(prefers-color-scheme:light)">
+    <!-- white favicons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/logo/favicon/w/apple-icon-57x57.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/logo/favicon/w/apple-icon-60x60.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/logo/favicon/w/apple-icon-72x72.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/logo/favicon/w/apple-icon-76x76.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/logo/favicon/w/apple-icon-114x114.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/logo/favicon/w/apple-icon-120x120.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/logo/favicon/w/apple-icon-144x144.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/logo/favicon/w/apple-icon-152x152.png" media="(prefers-color-scheme:dark)">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo/favicon/w/apple-icon-180x180.png" media="(prefers-color-scheme:dark)">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/logo/favicon/w/android-icon-192x192.png" media="(prefers-color-scheme:dark)">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo/favicon/w/favicon-32x32.png" media="(prefers-color-scheme:dark)">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo/favicon/w/favicon-96x96.png" media="(prefers-color-scheme:dark)">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo/favicon/w/favicon-16x16.png" media="(prefers-color-scheme:dark)">
+    <meta name="msapplication-TileImage" content="assets/img/logo/favicon/w/ms-icon-144x144.png" media="(prefers-color-scheme:dark)">
+    <!-- no pref favicons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/logo/favicon/b/apple-icon-57x57.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/logo/favicon/b/apple-icon-60x60.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/logo/favicon/b/apple-icon-72x72.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/logo/favicon/b/apple-icon-76x76.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/logo/favicon/b/apple-icon-114x114.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/logo/favicon/b/apple-icon-120x120.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/logo/favicon/b/apple-icon-144x144.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/logo/favicon/b/apple-icon-152x152.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo/favicon/b/apple-icon-180x180.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/logo/favicon/b/android-icon-192x192.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo/favicon/b/favicon-32x32.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo/favicon/b/favicon-96x96.png" media="(prefers-color-scheme:no-preference)">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo/favicon/b/favicon-16x16.png" media="(prefers-color-scheme:no-preference)">
+    <meta name="msapplication-TileImage" content="assets/img/logo/favicon/b/ms-icon-144x144.png" media="(prefers-color-scheme:no-preference)">
+    <!-- else -->
+    <link rel="manifest" href="assets/img/logo/favicon/b/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="assets/img/logo/favicon/ms-icon-144x144.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="description" content="YAOSCP - Yet Another Open Source Cloud Project">
     <meta name="author" content="tholeb">
